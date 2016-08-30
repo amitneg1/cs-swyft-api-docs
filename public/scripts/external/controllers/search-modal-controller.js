87fab688783404ede000bf54d8fdc4e1424b3108
Amit Gupta <amit.gupta@monotype.com>
2016-08-30 17:59:39 +0530

    Search plugin

--- public/scripts/external/controllers/search-modal-controller.js
+++ public/scripts/external/controllers/search-modal-controller.js
@@ -1,5 +1,13 @@
 var app = angular.module('swyftDoc');
 
 app.controller('searchModalController', ['$scope', '$uibModalInstance', function($scope, $uibModalInstance){
-	$scope.msg = "Hello World";
+	
+	$scope.queryText = "";
+
+	$scope.cancel = function () {
+	    $uibModalInstance.dismiss('cancel');
+	};
+	$scope.clearSearch = function(){
+		$scope.queryText = "";
+	}
 }])
